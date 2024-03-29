610d90b1ba621fb00241dd5f2684840282dc56df
Piotr Urbanski <piotr.s.urbanski@gmail.com>
2024-03-29 09:22:07 +0100

    Spil out model deserialization factory interface

--- Runtime/Scripts/Model/IModelFactory.cs
+++ Runtime/Scripts/Model/IModelFactory.cs
@@ -7,53 +7,52 @@ namespace pindwin.umvr.Model
 		IModel CreateDefault();
 		Type ModelType { get; }
 	}
+	
+	public interface IModelDeserializationFactory<out TModel>
+	{
+		TModel Create<TDeserializationPayload>(TDeserializationPayload deserializationPayload);
+	}
 
-	public interface IModelFactory<out TModel> : IModelFactory
+	public interface IModelFactory<out TModel> : IModelFactory, IModelDeserializationFactory<TModel>
 		where TModel : IModel
 	{
 		TModel Create(Id id);
 		TModel Create();
-		TModel Create<TDeserializationPayload>(TDeserializationPayload deserializationPayload);
 	}
 
-	public interface IModelFactory<out TModel, in TParam0> : IModelFactory
+	public interface IModelFactory<out TModel, in TParam0> : IModelFactory, IModelDeserializationFactory<TModel>
 		where TModel : IModel
 	{
 		TModel Create(Id id, TParam0 param0);
 		TModel Create(TParam0 param0);
-		TModel Create<TDeserializationPayload>(TDeserializationPayload deserializationPayload);
 	}
 
-	public interface IModelFactory<out TModel, in TParam0, in TParam1> : IModelFactory
+	public interface IModelFactory<out TModel, in TParam0, in TParam1> : IModelFactory, IModelDeserializationFactory<TModel>
 		where TModel : IModel
 	{
 		TModel Create(Id id, TParam0 param0, TParam1 param1);
 		TModel Create(TParam0 param0, TParam1 param1);
-		TModel Create<TDeserializationPayload>(TDeserializationPayload deserializationPayload);
 	}
 
-	public interface IModelFactory<out TModel, in TParam0, in TParam1, in TParam2> : IModelFactory
+	public interface IModelFactory<out TModel, in TParam0, in TParam1, in TParam2> : IModelFactory, IModelDeserializationFactory<TModel>
 		where TModel : IModel
 	{
 		TModel Create(Id id, TParam0 param0, TParam1 param1, TParam2 param2);
 		TModel Create(TParam0 param0, TParam1 param1, TParam2 param2);
-		TModel Create<TDeserializationPayload>(TDeserializationPayload deserializationPayload);
 	}
 
-	public interface IModelFactory<out TModel, in TParam0, in TParam1, in TParam2, in TParam3> : IModelFactory
+	public interface IModelFactory<out TModel, in TParam0, in TParam1, in TParam2, in TParam3> : IModelFactory, IModelDeserializationFactory<TModel>
 		where TModel : IModel
 	{
 		TModel Create(Id id, TParam0 param0, TParam1 param1, TParam2 param2, TParam3 param3);
 		TModel Create(TParam0 param0, TParam1 param1, TParam2 param2, TParam3 param3);
-		TModel Create<TDeserializationPayload>(TDeserializationPayload deserializationPayload);
 	}
 
 	public interface
-		IModelFactory<out TModel, in TParam0, in TParam1, in TParam2, in TParam3, in TParam4> : IModelFactory
+		IModelFactory<out TModel, in TParam0, in TParam1, in TParam2, in TParam3, in TParam4> : IModelFactory, IModelDeserializationFactory<TModel>
 		where TModel : IModel
 	{
 		TModel Create(Id id, TParam0 param0, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4);
 		TModel Create(TParam0 param0, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4);
-		TModel Create<TDeserializationPayload>(TDeserializationPayload deserializationPayload);
 	}
 }
